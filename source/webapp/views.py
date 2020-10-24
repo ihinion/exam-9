5e0e29456bd442804f7abb39dc30baf6844a8c8e
ilia <jamiashvili@gmail.com>
2020-10-24 13:52:30 +0600

    Added permission checks for photo views

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from .forms import PhotoForm, PhotoFormUpdate
 
 from webapp.models import Photo
@@ -18,7 +19,7 @@ class IndexView(ListView):
     ordering = ['-created_at']
 
 
-class PhotoCreateView(CreateView):
+class PhotoCreateView(LoginRequiredMixin, CreateView):
     template_name = 'photos/photo_create.html'
     form_class = PhotoForm
     model = Photo
@@ -30,16 +31,24 @@ class PhotoCreateView(CreateView):
         return redirect('webapp:photo_detail', pk=photo.pk)
 
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(UserPassesTestMixin, UpdateView):
     template_name = 'photos/photo_update.html'
     form_class = PhotoFormUpdate
     model = Photo
+
+    def test_func(self):
+        return self.request.user.has_perm('webapp.change_photo') or \
+            self.get_object().author == self.request.user
 
     def get_success_url(self):
         return reverse('webapp:photo_detail', kwargs={'pk': self.object.pk})
 
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(UserPassesTestMixin, DeleteView):
     template_name = 'photos/photo_delete.html'
     model = Photo
     success_url = reverse_lazy('webapp:index')
+    
+    def test_func(self):
+        return self.request.user.has_perm('webapp.delete_photo') or \
+            self.get_object().author == self.request.user
