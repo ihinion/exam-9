22413c38c7e6a254d51c9dba3f51c21426e025c5
ilia <jamiashvili@gmail.com>
2020-10-24 11:44:06 +0600

    Created models for Photos and Favorites, migrated

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    picture = models.ImageField(upload_to='pics', verbose_name='Photo')
+    desc = models.CharField(max_length=200, verbose_name='Description')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                               related_name='photos', verbose_name='Author')
+
+
+class Favorite(models.Model):
+    author = models.ForeignKey(get_user_model(), related_name='photo_favorites', verbose_name='Author',
+                             on_delete=models.CASCADE)
+    photo = models.ForeignKey('webapp.Photo', on_delete=models.CASCADE, related_name='favorites',
+                                verbose_name='Photo')
+
+    def __str__(self):
+        return f'{self.author.username} - {self.photo.desc}'
+
